b9be68e58878201e444c16bfb0698dd4593b19bb
Filip Zdravkovic <fzdravkovic1@gmail.com>
2023-01-07 00:06:39 +0100

    Update domManipulation.js

--- domManipulation.js
+++ domManipulation.js
@@ -1,13 +1,13 @@
 Manipulating the DOM with JavaScript
 
 // Creating, adding and removing:
-document.createElement ( 'HTMLTag'); // Create an element
+document.createElement('HTMLTag'); // Create an element
 document.body.append(myElement); // Insert last
 document.body.prepend(myElement); // Insert first
 parentElement.insertBefore(newElement, existingElement); // Insert before 
 element.remove(); // Remove element
-element.insertAdjacentHTML // String to be parsed as HTML or XML and inserted into the tree at a specified position
-element.insertAdjacentText // Inserts a new text at the given position
+element.insertAdjacentHTML(); // String to be parsed as HTML or XML and inserted into the tree at a specified position
+element.insertAdjacentText(); // Inserts a new text at the given position
 
 // Content setting:
 element.innerHTML = "HTML content"; // Set HTML content inside element
@@ -15,16 +15,16 @@ element.innerText = "text content"; // Set text (preserves formatting)
 element.textContent = "text content"; // Set text (ignores formatting)
 
 // Working with attributes:
-element.getAttribute( 'attribute'); // Returns attribute value
-element.removeAttribute( 'attribute'); // Removes attribute
-element.setAttribute( 'attribute', 'value'); // Sets attribute with value
+element.getAttribute('attribute'); // Returns attribute value
+element.removeAttribute('attribute'); // Removes attribute
+element.setAttribute('attribute', 'value'); // Sets attribute with value
 element.id; // Returns element id
 element.id = 'value'; // Sets element id
 element.className = 'list'; // Set new class list
 element.classList; // Returns items in class list in array-like format
 element.classList.add('classItem'); // Add item to class list
 element.classList.remove('classItem'); // Remove item from class list
-element.classList.toggle( 'classItem'); // Toggle item in class list
+element.classList.toggle('classItem'); // Toggle item in class list
 
 // Editing styles:
 getComputedStyle(myElement); // Returns all styling of an element
